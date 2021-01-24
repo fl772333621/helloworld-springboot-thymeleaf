62c39c1b1c10340f03187de8f03ee58849fcf7d8
孟伟 <772333621@qq.com>
2021-01-24 13:05:42 +0800

    启用 SpringBoot Security, 默认登陆用户为 user, 默认登陆密码为日志(往上翻)中的 Using generated security password:XXX

--- src/main/java/com/mfanw/helloworld/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/mfanw/helloworld/thymeleaf/ThymeleafApplication.java
@@ -1,5 +1,7 @@
 package com.mfanw.helloworld.thymeleaf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class ThymeleafApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ThymeleafApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafApplication.class, args);
+        LOGGER.warn("启用 SpringBoot Security, 默认登陆用户为 user, 默认登陆密码为日志(往上翻)中的 Using generated security password:XXX");
     }
 
 }
